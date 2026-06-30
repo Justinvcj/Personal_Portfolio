ed517d7c44e20f247ef9ecbe74c4f8b962dbf82d
Justinvcj <23cs037@drngpit.ac.in>
2026-06-30 11:53:27 +0530

    Fix: Remove .vercel from tracking to allow Vercel to build from source

--- .vercel/output/functions/__server.func/_ssr/start-CmFEMW55.mjs
+++
@@ -1,5 +0,0 @@
-import { t as createStart } from "./ssr.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/start-CmFEMW55.js
-var startInstance = createStart(() => ({ requestMiddleware: [] }));
-//#endregion
-export { startInstance };
